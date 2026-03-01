8ed385830176783179c5de94345e2769ae83c87f
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2026-03-01 11:47:30 +0200

    Expose state encoding without a full graph instance

--- src/core/solver.ts
+++ src/core/solver.ts
@@ -40,7 +40,7 @@ export type SolutionInfo = {
 const MIN_VALUE = -32767 // Not -32768 to support unary minus
 const MAX_VALUE = 32767
 
-function keyspaceSize(root: State): number {
+export function keyspaceSize(root: State): number {
   const numMoves = stonesCount(root.logicalArea)
   let result = 1
 
@@ -70,6 +70,123 @@ function keyspaceSize(root: State): number {
   return result
 }
 
+// Encode root's child state into a number
+export function encode(root: State, state: State, moves?: Stones[]): number {
+  if (moves === undefined) {
+    moves = dots(root.logicalArea)
+    moves.push(emptyStones())
+  }
+  let result = 0
+  result = 2 * result + state.passes
+  result = 3 * result + state.button + 1
+
+  result = (2 * Math.abs(root.koThreats) + 1) * result + state.koThreats
+  let player = state.player
+  let opponent = state.opponent
+  if (state.whiteToPlay !== root.whiteToPlay) {
+    player = state.opponent
+    opponent = state.player
+  }
+  result =
+    (stonesCount(stonesAnd(root.external, root.opponent)) + 1) * result +
+    stonesCount(stonesAnd(root.external, opponent))
+  result =
+    (stonesCount(stonesAnd(root.external, root.player)) + 1) * result +
+    stonesCount(stonesAnd(root.external, player))
+
+  result *= moves.length
+  for (let i = 0; i < moves.length; ++i) {
+    if (equals(moves[i]!, state.ko)) {
+      result += i
+      break
+    }
+  }
+
+  for (let i = 0; i < moves.length - 1; ++i) {
+    result *= 3
+    if (overlaps(moves[i]!, state.player)) {
+      result += 1
+    } else if (overlaps(moves[i]!, state.opponent)) {
+      result += 2
+    }
+  }
+
+  result = 2 * result + Number(state.whiteToPlay)
+
+  return result
+}
+
+// Decode a number into a game state
+export function decode(root: State, key: number, moves?: Stones[]): State {
+  if (moves === undefined) {
+    moves = dots(root.logicalArea)
+    moves.push(emptyStones())
+  }
+  const result = new State(root)
+  const effectiveArea = stonesAnd(result.logicalArea, stonesNot(result.external))
+  subtract(result.player, effectiveArea)
+  subtract(result.opponent, effectiveArea)
+
+  let m = 2
+  let n = key % m
+  key = (key - n) / m
+  result.whiteToPlay = !!n
+
+  if (result.whiteToPlay !== root.whiteToPlay) {
+    ;[result.player, result.opponent] = [result.opponent, result.player]
+  }
+
+  for (let i = moves.length - 2; i >= 0; --i) {
+    n = key % 3
+    key = (key - n) / 3
+    if (n === 1) {
+      merge(result.player, moves[i]!)
+    } else if (n === 2) {
+      merge(result.opponent, moves[i]!)
+    }
+  }
+
+  m = moves.length
+  n = key % m
+  key = (key - n) / m
+  result.ko = clone(moves[n]!)
+
+  const playerExternal = stonesAnd(root.external, root.player)
+  m = stonesCount(playerExternal) + 1
+  n = key % m
+  key = (key - n) / m
+  witherBy(playerExternal, m - 1 - n)
+
+  const opponentExternal = stonesAnd(root.external, root.opponent)
+  m = stonesCount(opponentExternal) + 1
+  n = key % m
+  key = (key - n) / m
+  witherBy(opponentExternal, m - 1 - n)
+
+  if (result.whiteToPlay === root.whiteToPlay) {
+    merge(result.player, playerExternal)
+    merge(result.opponent, opponentExternal)
+  } else {
+    merge(result.opponent, playerExternal)
+    merge(result.player, opponentExternal)
+  }
+
+  m = 2 * Math.abs(root.koThreats) + 1
+  n = key % m
+  key = (key - n) / m
+  result.koThreats = n - Math.abs(root.koThreats)
+
+  m = 3
+  n = key % m
+  key = (key - n) / m
+  result.button = n - 1
+
+  result.passes = key % 2
+  key -= result.passes
+
+  return result
+}
+
 export class Graph {
   root: State
   moves: Stones[]
@@ -88,122 +205,13 @@ export class Graph {
     this.highs.fill(MAX_VALUE)
   }
 
-  // Encode root's child state into a number
-  encode(state: State) {
-    let result = 0
-    result = 2 * result + state.passes
-    result = 3 * result + state.button + 1
-
-    result = (2 * Math.abs(this.root.koThreats) + 1) * result + state.koThreats
-    let player = state.player
-    let opponent = state.opponent
-    if (state.whiteToPlay !== this.root.whiteToPlay) {
-      player = state.opponent
-      opponent = state.player
-    }
-    result =
-      (stonesCount(stonesAnd(this.root.external, this.root.opponent)) + 1) * result +
-      stonesCount(stonesAnd(this.root.external, opponent))
-    result =
-      (stonesCount(stonesAnd(this.root.external, this.root.player)) + 1) * result +
-      stonesCount(stonesAnd(this.root.external, player))
-
-    result *= this.moves.length
-    for (let i = 0; i < this.moves.length; ++i) {
-      if (equals(this.moves[i]!, state.ko)) {
-        result += i
-        break
-      }
-    }
-
-    for (let i = 0; i < this.moves.length - 1; ++i) {
-      result *= 3
-      if (overlaps(this.moves[i]!, state.player)) {
-        result += 1
-      } else if (overlaps(this.moves[i]!, state.opponent)) {
-        result += 2
-      }
-    }
-
-    result = 2 * result + Number(state.whiteToPlay)
-
-    return result
-  }
-
-  // Decode a number into a game state
-  decode(key: number) {
-    const result = new State(this.root)
-    const effectiveArea = stonesAnd(result.logicalArea, stonesNot(result.external))
-    subtract(result.player, effectiveArea)
-    subtract(result.opponent, effectiveArea)
-
-    let m = 2
-    let n = key % m
-    key = (key - n) / m
-    result.whiteToPlay = !!n
-
-    if (result.whiteToPlay !== this.root.whiteToPlay) {
-      ;[result.player, result.opponent] = [result.opponent, result.player]
-    }
-
-    for (let i = this.moves.length - 2; i >= 0; --i) {
-      n = key % 3
-      key = (key - n) / 3
-      if (n === 1) {
-        merge(result.player, this.moves[i]!)
-      } else if (n === 2) {
-        merge(result.opponent, this.moves[i]!)
-      }
-    }
-
-    m = this.moves.length
-    n = key % m
-    key = (key - n) / m
-    result.ko = clone(this.moves[n]!)
-
-    const playerExternal = stonesAnd(this.root.external, this.root.player)
-    m = stonesCount(playerExternal) + 1
-    n = key % m
-    key = (key - n) / m
-    witherBy(playerExternal, m - 1 - n)
-
-    const opponentExternal = stonesAnd(this.root.external, this.root.opponent)
-    m = stonesCount(opponentExternal) + 1
-    n = key % m
-    key = (key - n) / m
-    witherBy(opponentExternal, m - 1 - n)
-
-    if (result.whiteToPlay === this.root.whiteToPlay) {
-      merge(result.player, playerExternal)
-      merge(result.opponent, opponentExternal)
-    } else {
-      merge(result.opponent, playerExternal)
-      merge(result.player, opponentExternal)
-    }
-
-    m = 2 * Math.abs(this.root.koThreats) + 1
-    n = key % m
-    key = (key - n) / m
-    result.koThreats = n - Math.abs(this.root.koThreats)
-
-    m = 3
-    n = key % m
-    key = (key - n) / m
-    result.button = n - 1
-
-    result.passes = key % 2
-    key -= result.passes
-
-    return result
-  }
-
   // Returns `false` when done
   iterate(): boolean {
     let didChange = false
     for (let key = 0; key < this.lows.length; ++key) {
       let low = this.lows[key]!
       let high = MIN_VALUE
-      const parent = this.decode(key)
+      const parent = decode(this.root, key, this.moves)
       for (const move of this.moves) {
         const child = new State(parent)
         const r = child.makeMove(move)
@@ -219,7 +227,7 @@ export class Graph {
           low = Math.max(low, childScore)
           high = Math.max(high, childScore)
         } else {
-          const childKey = this.encode(child)
+          const childKey = encode(this.root, child, this.moves)
           low = Math.max(low, -this.highs[childKey]!)
           high = Math.max(high, -this.lows[childKey]!)
         }
@@ -234,7 +242,7 @@ export class Graph {
   }
 
   getValueRange(state: State): [number, number] {
-    const key = this.encode(state)
+    const key = encode(this.root, state, this.moves)
     return [this.lows[key]! / SCORE_Q7_SCALE, this.highs[key]! / SCORE_Q7_SCALE]
   }
 
